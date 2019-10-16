befee28c2017cf2c5f9204890bf8d16f6586cca5
Juan Rivera <jrivera@primalplatoon.com>
2019-10-16 06:59:55 -0400

    Fixed most of the functionality.  Questions advance through to the 4th question.  Need to add 6 more questions and fix timer.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -11,7 +11,8 @@ $(document).ready(function () {
             if (tenSecTimer <= -0.5) {
                 clearInterval(resetTimer);
                 document.getElementById("timer").innerHTML = ":00";
-                document.getElementById("trivia").innerHTML = "What's your favorite scary movie?";
+                // Code below this line is to test the countdown timer#s ability to trigger an action upon countdown
+                // document.getElementById("trivia").innerHTML = "What's your favorite scary movie?";
 
             }
         }, 1000);
@@ -20,17 +21,6 @@ $(document).ready(function () {
         console.log(tenSecTimer);
     });
 
-    // Trying to get on click button to work for dynamic jQuery button
-    // // $(jQuery("#cA1")).on('click', function () {
-    //     console.log("Correct");
-    //     correctAnswer1();
-    // });
-
-
-
-
-
-
 
     function startGame() {
         document.getElementById("disappear").innerHTML = "";
@@ -38,48 +28,152 @@ $(document).ready(function () {
         document.getElementById("choices").innerHTML = choices1;
         // document.getElementById("choices").innerHTML = questionCount;
 
-
     }
-
-
-
-    // Not functioning...commenting out until issue is fixed
-    function correctAnswer1() {
-        document.getElementById("msg3").innerHTML = "Correct";
+    function correctAnswer() {
+        document.getElementById("msg2").innerHTML = "Correct";
         document.getElementById("choices").innerHTML = "";
-        document.getElementById("scre").innerHTML = "";
-
-        // document.getElementById("cA1").addEventListener('click', correctAnswer1());
-        // $( "#other" ).click(function() {
-        //     $( "#target" ).click();
-        //   });
+        //Commenting out Score variable until I get it to work
+        // document.getElementById("score").innerHTML = "";
 
     }
 
+    function incorrectAnswer() {
+        document.getElementById("msg2").innerHTML = "Sorry. That's incorrect.";
+        document.getElementById("choices").innerHTML = "";
 
+    }
 
 
     let questions = ["1. &nbsp Avocados, walnuts, and almonds are good sources of?",
         "2. &nbsp Frequent consumption of sugar-sweetened beverages in non-active individuals is associated with:",
-        "3. &nbsp An excellent source of carbohydrates for a long race includes:",
+        "3. &nbsp An excellent source of protein for lean muscle mass includes:",
         "4. &nbsp Sweet Potatoes and carrots are great sources of?"];
-    //Fix the space between btn-primary.  That's why the buttons arent renering
-    let choices1 = [`<button type="button" class="btn btn-primary" id="cA1">Healthy Fats</button>`,
-        "<button type="button" class="btn btn - primary" id="iA1">Unwanted weight gain</button>",
-        "<button type="button" class="btn btn - primary" id="iA1">Bad Calories</button>",
-        "<button type="button" class="btn btn - primary" id="iA1">Good Carbs</button>"];
+    //ID names cA and iA stand for "correct answer" & incorrect answer, respectively.
+    let choices1 = ['<button type="button" class="btn-primary" id="cA1">Healthy Fats</button>',
+        '<button type="button" class="btn-primary" id="iA1">Unwanted weight gain</button>',
+        '<button type="button" class="btn-primary" id="iA1">Bad Calories</button>',
+        '<button type="button" class="btn-primary" id="iA1">Good Carbs</button>'];
+
+    let choices2 = ['<button type="button" class="btn-primary" id="iA2">Lean muscle mass</button>',
+        '<button type="button" class="btn-primary" id="iA2">Improved focus</button>',
+        '<button type="button" class="btn-primary" id="iA2">Weightloss</button>',
+        '<button type="button" class="btn-primary" id="cA2">Obesity</button>'];
+
+    let choices3 = ['<button type="button" class="btn-primary" id="iA3">donuts & other pastries</button>',
+        '<button type="button" class="btn-primary" id="cA3">lean meats & eggs</button>',
+        '<button type="button" class="btn-primary" id="iA3">Coffee</button>',
+        '<button type="button" class="btn-primary" id="iA3">Energy drinks</button>'];
+
+    let choices4 = ['<button type="button" class="btn-primary" id="cA4">Vitamin A, to support good vision.</button>',
+        '<button type="button" class="btn-primary" id="iA4">Vitamin D, to support bone health</button>',
+        '<button type="button" class="btn-primary" id="iA4">Protein, for lean muscle mass</button>',
+        '<button type="button" class="btn-primary" id="iA4">Caffeine, for extra energy</button>'];
+
+    let score = 0;
 
     // let questionCount = 0;
     // questionCount++
 
-    // let score = 0;
-    // score++;
+
 
     $("#choices").on("click", "#cA1", function () {
-        // $("#choices").click();
+        correctAnswer();
+        document.getElementById("msg3").innerHTML = '<button class="btn btn-success btn-lg" role="button" id="nextQ">Next</button>';
+        score++
         console.log('hitting new clicker')
-        alert('hitting ca1 id')
+        // alert('hitting ca1 id')
+        // alert("Your score:" + score);
     })
+
+    $("#choices").on("click", "#iA1", function () {
+        incorrectAnswer();
+        document.getElementById("msg3").innerHTML = '<button class="btn btn-success btn-lg" role="button" id="nextQ">Next</button>';
+        console.log('Sorry. Wrong answer.');
+
+    })
+
+    $("#choices").on("click", "#cA2", function () {
+        correctAnswer();
+        document.getElementById("msg3").innerHTML = '<button class="btn btn-success btn-lg" role="button" id="nextQ2">Next</button>';
+        score++
+        console.log('hitting new clicker')
+        // alert('hitting ca1 id')
+        // alert("Your score:" + score);
+    })
+
+    $("#choices").on("click", "#iA2", function () {
+        incorrectAnswer();
+        document.getElementById("msg3").innerHTML = '<button class="btn btn-success btn-lg" role="button" id="nextQ2">Next</button>';
+        console.log('Sorry. Wrong answer.');
+
+    })
+
+    $("#choices").on("click", "#cA3", function () {
+        correctAnswer();
+        document.getElementById("msg3").innerHTML = '<button class="btn btn-success btn-lg" role="button" id="nextQ3">Next</button>';
+        score++
+        console.log('hitting new clicker')
+        // alert('hitting ca1 id')
+        // alert("Your score:" + score);
+    })
+
+    $("#choices").on("click", "#iA3", function () {
+        incorrectAnswer();
+        document.getElementById("msg3").innerHTML = '<button class="btn btn-success btn-lg" role="button" id="nextQ3">Next</button>';
+        console.log('Sorry. Wrong answer.');
+
+    })
+    $("#choices").on("click", "#cA4", function () {
+        correctAnswer();
+        document.getElementById("msg3").innerHTML = '<button class="btn btn-success btn-lg" role="button" id="nextQ4">Next</button>';
+        score++
+        console.log('hitting new clicker')
+        // alert('hitting ca1 id')
+        // alert("Your score:" + score);
+    })
+
+    $("#choices").on("click", "#iA4", function () {
+        incorrectAnswer();
+        document.getElementById("msg3").innerHTML = '<button class="btn btn-success btn-lg" role="button" id="nextQ4">Next</button>';
+        console.log('Sorry. Wrong answer.');
+
+    })
+
+
+    $("#msg3").on("click", "#nextQ", function () {
+        document.getElementById("msg2").innerHTML = "";
+        document.getElementById("question1").innerHTML = questions[1];
+        document.getElementById("choices").innerHTML = choices2;
+        console.log('Moving to question 2.');
+
+    })
+
+    $("#msg3").on("click", "#nextQ2", function () {
+        document.getElementById("msg2").innerHTML = "";
+        document.getElementById("question1").innerHTML = questions[2];
+        document.getElementById("choices").innerHTML = choices3;
+        console.log('Moving to question 3.');
+        console.log('test for Q3 next button');
+        // alert('hitting ca1 id')
+        // alert("Your score:" + score);
+    })
+
+    $("#msg3").on("click", "#nextQ3", function () {
+        document.getElementById("msg2").innerHTML = "";
+        document.getElementById("question1").innerHTML = questions[3];
+        document.getElementById("choices").innerHTML = choices4;
+        console.log('This is question 4.');
+        console.log('test for Q4 next button');
+        // alert('hitting ca1 id')
+        // alert("Your score:" + score);
+    })
+
+
+
+
+
+
+
 
 
 
